10892ddf09e6983ed963729449a11a14447620e8
Tom Bailey <bailey@ficonsulting.com>
2017-10-10 13:21:07 -0400

    parsing of free-form text in Violations field

--- food_analysis.R
+++ food_analysis.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(lubridate)
+library(tm)
+library(stringr)
+library(gtools)
 
 colClasses <- c("integer", rep("character", 2), "integer",
                 rep("factor", 2), rep("character", 2), "factor",
@@ -52,17 +55,22 @@ ggplot(data = food) +
 
 # What is distribution of results for each level of risk? (get rid of "All" and "" in
 # Risk variable)
-
 food %>%
   filter(!(Risk %in% c("All", ""))) %>%
-  group_by(Risk) %>%
-  count(Results)  
+  count(Risk, Results)
+
+# try visualizing with tile plot
+food %>%
+  filter(!(Risk %in% c("All", ""))) %>%
+  count(Risk, Results) %>%
+  ggplot(mapping = aes(x = Risk, y = Results)) +
+    geom_tile(mapping = aes(fill = n))
 
 ggplot(data = food %>%
                 filter(!(Risk %in% c("All", "")))
        ) +
           geom_bar(mapping = aes(x = Results)) +
-          facet_grid(Risk ~ .)
+          facet_grid(. ~ Risk)
 
 # those are raw numbers, but rates of failure, success, etc within each
 # Risk group would be better
@@ -100,5 +108,106 @@ ggplot(data = rates_rr2) +
 # medium and high risk. 
 
 
+### Violations field is free-form text; split and get words
+test <- strsplit(food$Violations[4], "[ |-]")
+test <- unlist(test)
+test <- gsub("[^[:alpha:]|']", "", test)
+test <- tolower(test)
+test <- test[!(test %in% c(stopwords("english"),""))]
+as.list(test)
 
+
+
+
+test <- food %>%
+  mutate(s = strsplit(Violations, "|", fixed = T))
+  
+test2 <- test %>%
+  unnest(s)
+
+
+# run into trouble when we do this b/c too many values b/c comments: appears multiple
+# times for some records ; eg, look at 20th record of test2 where it has "inspector comments:" as
+# well
+test3 <- test2 %>%
+  separate(s, into = c("var", "val"), sep = "Comments:")
+  
+# instead look for hyphen in front of comments to split, put * after space in case it 
+# appears as "-Comments:" instead of "- Comments:"; also use [Cc] in case Comments is
+# not capitalized. This increases record count from 156,065 to 576,711
+test3 <- test2 %>%
+  separate(s, into = c("var", "val"), sep = "- *[Cc]omments:") %>%
+  mutate_at(vars("var", "val"), funs(str_trim))
+
+test4 <- test3 %>%
+  spread(var, val)
+
+# we get Error: Duplicate identifiers for rows (181869, 181870),... why
+test3[c(181869, 181870), c("Inspection.ID", "DBA.Name", "var", "val")]  #appears there are multiple keys for this restaurant Cafe on the Grove
+food %>%
+  filter(DBA.Name == "CAFE ON THE GROVE") 
+
+food %>%
+  filter(DBA.Name == "CAFE ON THE GROVE") %>%
+  select(Inspection.Date, Violations) 
+# the inspection on 5/26/15 had multiple entries for 
+# 10. SEWAGE AND WASTE WATER DISPOSAL, NO BACK SIPHONAGE, CROSS  CONNECTION AND/OR BACK FLOW
+# which is causing the error we got above
+# How to get around this? One thought is do a group by on test3 DF for inspection ID and var, 
+# and merge val together for duplicate records. Record count drops from 576,711 to 569,002
+test4 <- test3 %>%
+  group_by_at(names(.)[names(.) != "val"]) %>%
+  summarize(val = paste(val, collapse = " ")) %>%
+  ungroup()   # need to ungroup or else many of the columns will still be grouped  
+
+# now look at the val field for the Inspection.ID that had the duplicate entries (1546425)
+t<-test4 %>% 
+  filter(Inspection.ID == 1546425) %>%
+  filter(grepl("10.", var))
+
+# The 2 entries for 10. SEWAGE AND WASTE WATER DISPOSAL... are now concatenated into 1 string 
+t$val
+t$Violations
+
+# now we can continue with the dplyr::spread() function
+test6 <- test4 %>%
+          spread(var, val, fill = NA)
+
+# names of new columns are incredibly long. just store numbers and save descriptions to 
+# separate data frame
+reqs <- names(test6)[grepl("\\d{1,2}\\.", names(test6))]
+nbr_matches <- str_match(reqs, "(\\d{1,2})\\.")  # look for number of 1 or 2 digits immediately followed by period, we will get a matrix with has the full match in 1st column, and then the group in the 2nd column
+req_nbrs <- as.numeric(nbr_matches[, 2]) # only take 2nd column
+req_descs <- str_trim(gsub("\\d{1,2}\\.", "", reqs))
+
+reqs <- data.frame(nbr = req_nbrs, desc = req_descs) %>%
+          arrange(nbr)
+
+# now clean up columns in test6 and reorder columns
+names(test6)[grepl("\\d{1,2}\\.", names(test6))] <- paste0("req", req_nbrs)
+req_cols <- mixedsort(names(test6)[grepl("\\d{1,2}", names(test6))])
+
+test7 <- test6[ , names(test6)[!(names(test6) %in% req_cols)]] %>%
+          cbind(test6 %>%
+                  select(one_of(req_cols))
+                )
+
+# our resulting data frame only has 124,812, but the original food DF has 156,065
+# what happened to these 31,253 records?
+# they dropped out when we used dplyr::spread() b/c Violations fld is empty 
+
+x <- food %>%
+      filter(!(Inspection.ID %in% test7$Inspection.ID)) %>%
+      select(Results, Violations)
+
+# look at the Results; violations NA b/c they either passed or were not inspected for some reason,
+# although some Failed or Passed w/Conditions but had no Violations listed, which is interesting
+x %>%
+  filter(Results %in% c("Fail", "Pass w/ Conditions"), Violations == "") %>%
+  count()
+
+# add them back to the DF we created with the req columns
+test8 <- food %>%
+            filter(!(Inspection.ID %in% test7$Inspection.ID)) %>%
+            bind_rows(test7)
 
