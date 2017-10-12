110398e8770b7501134104c393b0dca76e528f66
Tom Bailey <bailey@ficonsulting.com>
2017-10-12 15:44:36 -0400

    finished parsing violations field

--- food_analysis.R
+++ food_analysis.R
@@ -1,20 +1,17 @@
 library(tidyverse)
 library(lubridate)
-library(tm)
 library(stringr)
 library(gtools)
+library(XML)
+library(RCurl)
 
-colClasses <- c("integer", rep("character", 2), "integer",
-                rep("factor", 2), rep("character", 2), "factor",
-                "integer", "character", rep("factor", 2),
-                "character", rep("numeric",2), "character")
 
-food <- read.csv("Food_Inspections.csv", colClasses = colClasses)
+food <- read_csv("Food_Inspections.csv")
 
 food <- food %>%
-          mutate(Inspection.Date = as.Date(Inspection.Date, 
-                                           format = "%m/%d/%Y")
-                 )
+          mutate(`Inspection Date` = parse_date(`Inspection Date`, "%m/%d/%Y")) %>%
+          mutate_at(vars(Risk, Results), as.factor)
+
 
 ## Look for variation w/in variables ##
 # Results var
@@ -108,74 +105,79 @@ ggplot(data = rates_rr2) +
 # medium and high risk. 
 
 
-### Violations field is free-form text; split and get words
-test <- strsplit(food$Violations[4], "[ |-]")
-test <- unlist(test)
-test <- gsub("[^[:alpha:]|']", "", test)
-test <- tolower(test)
-test <- test[!(test %in% c(stopwords("english"),""))]
-as.list(test)
+food_parsed <- food %>%
+                mutate(vio_split = strsplit(Violations, "|", fixed = T)) %>%
+                unnest(vio_split)
 
-
-
-
-test <- food %>%
-  mutate(s = strsplit(Violations, "|", fixed = T))
-  
-test2 <- test %>%
-  unnest(s)
-
-
-# run into trouble when we do this b/c too many values b/c comments: appears multiple
-# times for some records ; eg, look at 20th record of test2 where it has "inspector comments:" as
-# well
-test3 <- test2 %>%
-  separate(s, into = c("var", "val"), sep = "Comments:")
+# We would continue this dplyr chain, but want to illustrate the error we will get
+# when we try to use the dplyr separate() function
+# run into trouble when we do this b/c too many values b/c comments: appears multiple times 
+# for some records ; eg, look at 20th record of test2 where it has "inspector comments:" as well
+food_parsed <- food_parsed %>%
+                separate(vio_split, 
+                         into = c("var", "val"), 
+                         sep = "Comments:")
   
 # instead look for hyphen in front of comments to split, put * after space in case it 
 # appears as "-Comments:" instead of "- Comments:"; also use [Cc] in case Comments is
 # not capitalized. This increases record count from 156,065 to 576,711
-test3 <- test2 %>%
-  separate(s, into = c("var", "val"), sep = "- *[Cc]omments:") %>%
-  mutate_at(vars("var", "val"), funs(str_trim))
+food_parsed <- food %>%
+                mutate(vio_split = strsplit(Violations, "|", fixed = T)) %>%
+                unnest(vio_split) %>%
+                separate(vio_split, 
+                         into = c("var", "val"), 
+                         sep = "- *[Cc]omments:") %>%
+                mutate_at(vars("var", "val"), funs(str_trim))
 
-test4 <- test3 %>%
-  spread(var, val)
+# Let's break the chain here, because we're going to get an error when we try to run dplyr's
+# spread() function, which unstacks the data and gives each distinct value in var its own column
+food_parsed <- food_parsed %>%
+                spread(var, val)
 
-# we get Error: Duplicate identifiers for rows (181869, 181870),... why
-test3[c(181869, 181870), c("Inspection.ID", "DBA.Name", "var", "val")]  #appears there are multiple keys for this restaurant Cafe on the Grove
+# we get Error: Duplicate identifiers for rows (314334, 314336),... why? 
+# Let's look at the first two rows they mention
+# appears there are multiple keys for this restaurant JEWEL FOOD STORE #3030 for Inspection ID 1296681
+food_parsed %>%
+  filter(row_number() %in% c(314334, 314336)) %>%
+  select(`Inspection ID`, `DBA Name`, var, val) %>%
+  print(width = Inf)
+ 
+
+# Let's look at the original data frame for this Inspection ID
 food %>%
-  filter(DBA.Name == "CAFE ON THE GROVE") 
+  filter(`Inspection ID` == 1296681) %>%
+  select(`Inspection Date`, Violations) %>%
+  print(width = Inf)
 
-food %>%
-  filter(DBA.Name == "CAFE ON THE GROVE") %>%
-  select(Inspection.Date, Violations) 
-# the inspection on 5/26/15 had multiple entries for 
-# 10. SEWAGE AND WASTE WATER DISPOSAL, NO BACK SIPHONAGE, CROSS  CONNECTION AND/OR BACK FLOW
+# the inspection on 2013-11-12 had multiple entries for 
+# 1. SOURCE SOUND CONDITION, NO SPOILAGE, FOODS PROPERLY LABELED, SHELLFISH TAGS IN PLACE
 # which is causing the error we got above
-# How to get around this? One thought is do a group by on test3 DF for inspection ID and var, 
-# and merge val together for duplicate records. Record count drops from 576,711 to 569,002
-test4 <- test3 %>%
-  group_by_at(names(.)[names(.) != "val"]) %>%
-  summarize(val = paste(val, collapse = " ")) %>%
-  ungroup()   # need to ungroup or else many of the columns will still be grouped  
+# How to get around this? One thought is do a group by on food_parsed for everything except val, 
+# and merge val together for duplicate records. 
+food_parsed <- food_parsed %>%
+                group_by_at(names(.)[names(.) != "val"]) %>%
+                summarize(val = paste(val, collapse = " ")) %>%
+                ungroup()   # need to ungroup or else many of the columns will still be grouped  
 
-# now look at the val field for the Inspection.ID that had the duplicate entries (1546425)
-t<-test4 %>% 
-  filter(Inspection.ID == 1546425) %>%
-  filter(grepl("10.", var))
-
-# The 2 entries for 10. SEWAGE AND WASTE WATER DISPOSAL... are now concatenated into 1 string 
-t$val
-t$Violations
+# Now look at Inspection.ID of 1296681, which is JEWEL FOOD STORE #3030 inspection on 2013-11-12 and
+# filter for records with key/var of 10.
+# Strings from two different records have now been concatenated into one
+food_parsed %>%
+  filter(`Inspection ID` == 1296681, grepl("1. SOURCE SOUND CONDITION", var)) %>%
+  select(val) %>%
+  print(width = Inf)
 
 # now we can continue with the dplyr::spread() function
-test6 <- test4 %>%
-          spread(var, val, fill = NA)
+food_parsed <- food_parsed %>%
+                spread(var, val, fill = NA)
+
+# drop NA column
+food_parsed <- food_parsed %>%
+                  select(-`<NA>`)
 
 # names of new columns are incredibly long. just store numbers and save descriptions to 
 # separate data frame
-reqs <- names(test6)[grepl("\\d{1,2}\\.", names(test6))]
+reqs <- names(food_parsed)[grepl("\\d{1,2}\\.", names(food_parsed))]
 nbr_matches <- str_match(reqs, "(\\d{1,2})\\.")  # look for number of 1 or 2 digits immediately followed by period, we will get a matrix with has the full match in 1st column, and then the group in the 2nd column
 req_nbrs <- as.numeric(nbr_matches[, 2]) # only take 2nd column
 req_descs <- str_trim(gsub("\\d{1,2}\\.", "", reqs))
@@ -183,31 +185,105 @@ req_descs <- str_trim(gsub("\\d{1,2}\\.", "", reqs))
 reqs <- data.frame(nbr = req_nbrs, desc = req_descs) %>%
           arrange(nbr)
 
-# now clean up columns in test6 and reorder columns
-names(test6)[grepl("\\d{1,2}\\.", names(test6))] <- paste0("req", req_nbrs)
-req_cols <- mixedsort(names(test6)[grepl("\\d{1,2}", names(test6))])
+# now clean up columns in food_parsed and reorder columns
+names(food_parsed)[grepl("\\d{1,2}\\.", names(food_parsed))] <- paste0("req", req_nbrs)
+req_cols <- mixedsort(names(food_parsed)[grepl("\\d{1,2}", names(food_parsed))])
 
-test7 <- test6[ , names(test6)[!(names(test6) %in% req_cols)]] %>%
-          cbind(test6 %>%
-                  select(one_of(req_cols))
-                )
+food_parsed <- food_parsed[!(names(food_parsed) %in% req_cols)] %>%
+                cbind(food_parsed %>%
+                        select(one_of(req_cols))
+                      )
 
-# our resulting data frame only has 124,812, but the original food DF has 156,065
-# what happened to these 31,253 records?
-# they dropped out when we used dplyr::spread() b/c Violations fld is empty 
 
-x <- food %>%
-      filter(!(Inspection.ID %in% test7$Inspection.ID)) %>%
-      select(Results, Violations)
+# # Data is now much tidier than it originally was, but we can still further parse 
+# # the individual req columns. Most values follow the format of the specific violation 
+# # which is almost always lowercase, followed by inspector's comments, which are almost always
+# # all CAPS. 
+# 
+# 
+# # write function that takes in value, and returns same value with "|" to indicate split point
+# # between the rule and what was observed
+# divide_req <- function(val) {
+#   words <- str_split(val, " |:|\\.")[[1]]
+#   words <- words[words != ""]
+#   
+#   if (sum(str_to_upper(words) == words & grepl("[[:alpha:]]", words)) == 0) 
+#     val
+#   else {
+#     rule <- str_trim(str_c(words[str_to_upper(words) != words], collapse = " "))
+#     observed <- str_trim(str_c(words[str_to_upper(words) == words], collapse = " "))
+#     str_c(observed, rule, sep = "|")
+#   }
+# }
 
-# look at the Results; violations NA b/c they either passed or were not inspected for some reason,
-# although some Failed or Passed w/Conditions but had no Violations listed, which is interesting
-x %>%
-  filter(Results %in% c("Fail", "Pass w/ Conditions"), Violations == "") %>%
-  count()
 
-# add them back to the DF we created with the req columns
-test8 <- food %>%
-            filter(!(Inspection.ID %in% test7$Inspection.ID)) %>%
-            bind_rows(test7)
+# Determine which inspections were serious, which were critical
+# sometimes we see "CITATION ISSUED SERIOUS", mostly just "SERIOUS VIOLATION/CITATION"
 
+get_severity <- function(val) {
+  severity <- NA_character_
+  
+  if(grepl("serious (violation|citation)", val, ignore.case = T))
+    severity <- "serious"
+  else if(grepl("critical (violation|citation)", val, ignore.case = T))
+    severity <- "critical"
+  
+  if(!is.na(severity)) {
+    sents <- str_split(val, "\\.")[[1]]
+    if(sum(grepl("(violation|citation).+serious", 
+                 sents, 
+                 ignore.case = T)) > 0)
+      
+      severity <- "serious"
+    
+    else if(sum(grepl("(violation|citation).+serious", 
+                      sents, 
+                      ignore.case = T)) > 0)
+      
+      severity <- "critical"
+  }
+  
+  severity
+  
+}
+
+
+is_corrected <- function(val) {
+  if(grepl("not (corrected|abated)", val, ignore.case = T)) 
+    FALSE
+  else if(grepl("corrected|abated", val, ignore.case = T))
+    TRUE
+}
+
+
+# More details on the violations can be found online
+url <- "https://www.cityofchicago.org/city/en/depts/"
+url <- str_c(url, "cdph/provdrs/inspections_and_permitting/svcs/")
+url <- str_c(url, "understand_healthcoderequirementsforfoodestablishments.html")
+
+resp <- getURL(url)
+tbl <- readHTMLTable(resp)
+violation_info <- tbl[[1]] 
+
+violation_info <- violation_info %>%
+                    set_names(c("vio_nbr", "standard", "type")) %>%
+                    
+
+
+
+
+
+
+
+
+# Of inspections that failed, how many had serious violations?
+food_parsed %>%
+  filter(Results == "Fail") %>%
+  select(one_of(req_cols)) %>%
+  map(~sum(grepl("serious violation", ., ignore.case = T)))
+
+food_parsed %>%
+  filter(Results == "Fail") %>%
+  select(one_of(req_cols)) %>%
+  map(~sum(grepl("critical violation", ., ignore.case = T))) %>%
+  unlist()
